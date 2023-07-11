c6c236db7a2b96a0a61b913a064d7ecc9b123bd1
jadon282930 <90097791+jadon282930@users.noreply.github.com>
2023-07-11 18:01:40 +0530

    Delete src directory

--- src/component/FilledButton/index.jsx
+++
@@ -1,9 +0,0 @@
-import BtnLoader from "../../hoc/BtnLoader";
-
-const FilledButton = ({ type="button", className="btn btn_gray", value="Clear", loading,loader_class, ...rest }) => {
-    return (
-        <button disabled={loading} {...rest} type={type} className={className}>{loading ? <BtnLoader loader_class={loader_class} /> : value}</button>
-    )
-};
-
-export  default FilledButton
--- src/redux/store/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from '../rootReducer';
-import axiosClient from '../../api/axiosClient';
-
-
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument(axiosClient)))
-
-export default store;
